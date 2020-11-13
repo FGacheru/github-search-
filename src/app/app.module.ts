846f8625d668ec38716281d2455d3f5c9bf1573f
b-ndunda <francisgacheru2001@gmail.com>
2020-11-13 22:30:17 +0300

    import HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+ 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { GithubServiceService } from './github-service.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { SearchComponent } from './search/search.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GithubServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubServiceService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 }
