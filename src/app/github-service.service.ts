c04f598f5e3552765926aef4c5c72aa1dc1da7f1
b-ndunda <francisgacheru2001@gmail.com>
2020-11-14 03:57:07 +0300

    add username on the url

--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { from } from 'rxjs';
+// import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class GithubServiceService {
   constructor(private http : HttpClient) { }
 getRepo(){
 const username = 'FGacheru'
-return this.http.get(`https://api.github.com/users/$(username)/repos`)
+return this.http.get(`https://api.github.com/users/FGacheru/repos?access_token=d35c852b854353bd3f700d1a3e39fee82fbf3c57`)
 
 }
 
