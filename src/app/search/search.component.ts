52101cd2456a08df181e4216a781361b8393fbed
b-ndunda <francisgacheru2001@gmail.com>
2020-11-16 11:17:53 +0300

    add reload home page to as FGacheru

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubServiceService } from '../github-service.service';
+import { Repos } from "../repos";
+import { Users } from "../users";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit{
+  users: Users;
+  repos: Repos;
+  Users = environment.apiKey
+ 
+  
+  constructor(private gitService : GithubServiceService){ }
+  findUser(term: string){
+    this.gitService.findUser(term).then(
+      ( success)=>{
+        this.users = this.gitService.users;
+      },
+      (error)=>{
+        
+      }
+     )
+     this.gitService.getUsernameRepo(term).then(
+       ( success)=>{
+         this.repos = this.gitService.repos;
+       
+         
+         
+         
+       },
+       (error)=>{
+         console.log(error)
+       }
+      )
+   }
+ngOnInit():void{
+  this.findUser('FGacheru') 
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+};
 
 }
