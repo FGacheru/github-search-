24cd8d1bda2325d959de5b11e2258bd31c7d3b2e
b-ndunda <francisgacheru2001@gmail.com>
2020-11-16 13:56:09 +0300

    add a background-color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { GithubServiceService } from './github-service.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'github-search';
+  constructor(){
 
-  Repos : any = [];
-  constructor(private gitService : GithubServiceService){
-    
   }
-ngOnInit():void{
-//   this.getGitRepo
-  // }
-  //  getGitRepo(){
-    this.gitService.getRepo().subscribe(repos => {
-    this.Repos = repos;
-    console.log(JSON.stringify(repos))
-    });
-  }
+  
 
-};
-
+}
