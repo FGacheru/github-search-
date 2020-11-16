a148131eb5403279743f7042dc2be67c7f23d2cc
b-ndunda <francisgacheru2001@gmail.com>
2020-11-16 11:14:11 +0300

    delete nav component import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{ FormsModule } from '@angular/forms'
  
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +8,26 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GithubServiceService } from './github-service.service';
+import { FormComponent } from './form/form.component';
+import { from } from 'rxjs';
+import { DateCountPipe } from './date-count.pipe';
+//import { GithubProfileComponent } from './github-profile/github-profile.component';
+//import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent
+  AppComponent,
+  SearchComponent,
+  FormComponent,
+  DateCountPipe
+//  GithubProfileComponent,
+  //NotFoundComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [GithubServiceService],
   bootstrap: [AppComponent]
